8bd2c7eee08a85011876df0a523c255846980bd0
uqkun987 <1011615704@qq.com>
2018-09-29 21:22:14 +0800

    第一次提交

---
+++ js/index.js
@@ -0,0 +1,15 @@
+function loginclick(){
+	var sing_in = document.getElementById("sing-in");
+	var sing_up = document.getElementById("sing-up");
+
+	sing_in.style.display = "block";
+	sing_up.style.display = "none";
+}
+
+function registerclick(){
+	var sing_in = document.getElementById("sing-in");
+	var sing_up = document.getElementById("sing-up");
+
+	sing_in.style.display = "none";
+	sing_up.style.display = "block";
+}
